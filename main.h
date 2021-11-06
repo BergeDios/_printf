b7f484880323f854b4852231431769aca77922c1
Mateo Victorica <3951@holbertonschool.com>
2021-11-06 13:15:27 -0700

    .h putchar y strlen

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _strlen(char *s);
+
+
+
+#endif
