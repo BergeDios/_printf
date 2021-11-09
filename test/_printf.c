3013c8cdcf037b07db79569876c0319c124a52aa
BergeDios <santiago.goyret@gmail.com>
2021-11-08 22:38:25 -0800

    22 not wotking

--- test/_printf.c
+++ test/_printf.c
@@ -8,27 +8,34 @@
 int _printf(const char *format, ...)
 {
 	int total_length = 0, f = 0, i = 0;
-	char *s_semifinal, *s_final, *k;
+	char *s_semifinal, *s_final;
 	va_list arguments;
-	char* (*func)(va_list);
+	int (*func)(va_list, char *, int, int);
 
-	va_start(arguments, format);
-	s_semifinal = malloc(sizeof(char) * 1024);
+	va_start(arguments, format), s_semifinal = malloc(sizeof(char) * 1024);
 	if (!s_semifinal)
 		return (-1);
-	for (; format && format[i]; i++, f++, total_length++)
+	if (!format || (format[i] == '%' && !format[i + 1]))
 	{
-		if (!format || !format[i] || (format[i] == '%' && !format[i + 1]))
+		free(s_semifinal);
+		return (-1);
+	}
+	for (; format[i]; i++, f++, total_length++)
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			s_final = realloc(s_semifinal, total_length);
+			write(1, s_final, total_length), free(s_final);
 			return (-1);
+		}
 		if (format[i] == '%' && (format[i + 1] == 'c'
 			|| format[i + 1] == 's' || format[i + 1] == '%'))
 		{
 			if (format[i + 1] != '%')
 			{
-				func = get_func(format[i + 1]), k = func(arguments);
-				total_length = (total_length + (_strlen(k)) - 1);
-				_strcat(s_semifinal, k);
-				f = (f + (_strlen(k)) - 1), free(k), i++;
+				func = get_func(format[i + 1]);
+				total_length = (func(arguments, s_semifinal, total_length, f) - 1);
+				f = total_length, i++;
 			}
 			else
 				s_semifinal[f] = '%', i++;
@@ -36,7 +43,7 @@ int _printf(const char *format, ...)
 		else
 			s_semifinal[f] = format[i];
 	}
-	s_final = realloc(s_semifinal, (total_length));
+	s_final = realloc(s_semifinal, total_length);
 	write(1, s_final, total_length);
 	va_end(arguments), free(s_final);
 	return (total_length);
