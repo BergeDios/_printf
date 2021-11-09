84a34be71d4d352aee1377674c9edd631d40fb8f
Mateo Victorica <3951@holbertonschool.com>
2021-11-09 08:27:21 -0800

    test2

---
+++ test2/get_func.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_func - selects correct function
+ * @s: character identifier
+ * Return: correct function
+ */
+int (*get_func(char s))(va_list)
+{
+	int i = 0;
+	types ide[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'\0', NULL}
+	};
+
+	while (ide[i].fi)
+	{
+		if (ide[i].fi == s)
+			return (ide[i].func);
+		i++;
+	}
+	return (NULL);
+}
